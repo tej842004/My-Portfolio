0b8016f2ec1a8ef8a7b315dc38aa36f3813b4856
Prashant Chauhan <imtej08@gmail.com>
2025-06-01 00:44:01 +0530

    Add a loading spinner

--- src/components/BlogDetail.tsx
+++ src/components/BlogDetail.tsx
@@ -1,4 +1,12 @@
-import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Heading,
+  HStack,
+  Image,
+  Spinner,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { useParams } from "react-router";
 import Prashant from "../assets/images/prash.jpg";
 import RCB from "../assets/images/rcb.webp";
@@ -6,51 +14,69 @@ import useBlog from "../hooks/useBlog";
 
 const BlogDetail = () => {
   const { id } = useParams();
-  const { data: blog } = useBlog(id!);
+  const { data: blog, isLoading, error } = useBlog(id!);
+
+  if (error) return null;
 
   return (
     <VStack align="stretch" spacing={6} mt={10}>
-      <Heading fontSize={{ base: "2xl", md: "4xl" }} lineHeight="short">
-        {blog?.title}
-      </Heading>
-
-      <HStack spacing={2} wrap="wrap">
-        <Image
-          src={Prashant}
-          alt="Author"
-          objectFit="cover"
-          boxSize="30px"
-          borderRadius="full"
-        />
-        <Text fontWeight="medium" fontSize={{ base: "xs", md: "md" }}>
-          {blog?.author}
-        </Text>
-        <Text color="gray.500" fontSize={{ base: "xs", md: "md" }}>
-          {blog?.createdAt ? new Date(blog.createdAt).toLocaleDateString() : ""}
-        </Text>
-        <Text color="gray.500" fontSize={{ base: "xs", md: "md" }}>
-          · {blog?.readTime} min read
-        </Text>
-      </HStack>
-
-      <Image
-        src={RCB}
-        alt="Blog Banner"
-        height="100%"
-        width="100%"
-        objectFit="cover"
-        borderRadius="2xl"
-      />
-
-      <VStack spacing={4} align="stretch">
-        <Text
-          fontSize={{ base: "md", md: "lg" }}
-          lineHeight="tall"
-          color="gray.250"
+      {isLoading ? (
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          height="50vh"
+          width="100%"
         >
-          {blog?.content}
-        </Text>
-      </VStack>
+          <Spinner />
+        </Box>
+      ) : (
+        <>
+          <Heading fontSize={{ base: "2xl", md: "4xl" }} lineHeight="short">
+            {blog?.title}
+          </Heading>
+
+          <HStack spacing={2} wrap="wrap">
+            <Image
+              src={Prashant}
+              alt="Author"
+              objectFit="cover"
+              boxSize="30px"
+              borderRadius="full"
+            />
+            <Text fontWeight="medium" fontSize={{ base: "xs", md: "md" }}>
+              {blog?.author}
+            </Text>
+            <Text color="gray.500" fontSize={{ base: "xs", md: "md" }}>
+              {blog?.createdAt
+                ? new Date(blog.createdAt).toLocaleDateString()
+                : ""}
+            </Text>
+            <Text color="gray.500" fontSize={{ base: "xs", md: "md" }}>
+              · {blog?.readTime} min read
+            </Text>
+          </HStack>
+
+          <Image
+            src={RCB}
+            alt="Blog Banner"
+            height="100%"
+            width="100%"
+            objectFit="cover"
+            borderRadius="2xl"
+          />
+
+          <VStack spacing={4} align="stretch">
+            <Text
+              fontSize={{ base: "md", md: "lg" }}
+              lineHeight="tall"
+              color="gray.250"
+            >
+              {blog?.content}
+            </Text>
+          </VStack>
+        </>
+      )}
     </VStack>
   );
 };
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,12 @@
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { Box, Heading, Spinner, Text } from "@chakra-ui/react";
 import { Link } from "react-router";
 import useBlogs from "../hooks/useBlogs";
 import SearchInput from "./SearchInput";
 
 const Home = () => {
-  const { data: blogs } = useBlogs();
+  const { data: blogs, isLoading, error } = useBlogs();
+
+  if (error) return null;
 
   return (
     <Box as="section" role="region" aria-label="Latest Posts" marginTop={10}>
@@ -15,32 +17,45 @@ const Home = () => {
         <SearchInput />
       </Box>
 
-      {blogs?.map((blog, index) => (
-        <Box as="article" key={index} mb={8} maxW="700px">
-          <Link to={`/detail/${blog._id}`}>
-            <Heading as="h2" size="lg" mb={2}>
-              {blog.title}
-            </Heading>
-          </Link>
-          <Text mb={4} noOfLines={1} color="gray.500" fontWeight="normal">
-            {blog.content}
-          </Text>
-          <Box
-            display="flex"
-            alignItems="center"
-            gap={6}
-            fontSize="sm"
-            color="gray.500"
-          >
-            <Text>
-              {blog.createdAt
-                ? new Date(blog.createdAt).toLocaleDateString()
-                : "Unknown date"}
-            </Text>
-            <Text>{blog.readTime} min read</Text>
-          </Box>
+      {isLoading && (
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          height="50vh"
+          width="100%"
+        >
+          <Spinner />
         </Box>
-      ))}
+      )}
+
+      {!isLoading &&
+        blogs?.map((blog, index) => (
+          <Box as="article" key={index} mb={8} maxW="700px">
+            <Link to={`/detail/${blog._id}`}>
+              <Heading as="h2" size="lg" mb={2}>
+                {blog.title}
+              </Heading>
+            </Link>
+            <Text mb={4} noOfLines={1} color="gray.500" fontWeight="normal">
+              {blog.content}
+            </Text>
+            <Box
+              display="flex"
+              alignItems="center"
+              gap={6}
+              fontSize="sm"
+              color="gray.500"
+            >
+              <Text>
+                {blog.createdAt
+                  ? new Date(blog.createdAt).toLocaleDateString()
+                  : "Unknown date"}
+              </Text>
+              <Text>{blog.readTime} min read</Text>
+            </Box>
+          </Box>
+        ))}
     </Box>
   );
 };
